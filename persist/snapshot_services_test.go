1e9e59601b3bb853456d773bed97c2742c400788
Nitin Kumar <nitinraj844126@gmail.com>
2026-08-27 09:26:59 +0530

    feat(persist): Snapshottable for IAM across AWS/Azure/GCP (#582 wave 3)

--- persist/snapshot_services_test.go
+++ persist/snapshot_services_test.go
@@ -23,9 +23,9 @@ func TestSnapshotServicesDiscovery(t *testing.T) {
 		services map[string]struct{}
 		want     []string
 	}{
-		{"aws", asSet(keysOf(cloudemu.NewAWS().SnapshotServices())), []string{"dynamodb", "ec2", "s3", "secretsmanager"}},
-		{"azure", asSet(keysOf(cloudemu.NewAzure().SnapshotServices())), []string{"blobstorage", "cosmosdb", "keyvault", "virtualmachines"}},
-		{"gcp", asSet(keysOf(cloudemu.NewGCP().SnapshotServices())), []string{"firestore", "gce", "gcs", "secretmanager"}},
+		{"aws", asSet(keysOf(cloudemu.NewAWS().SnapshotServices())), []string{"dynamodb", "ec2", "iam", "s3", "secretsmanager"}},
+		{"azure", asSet(keysOf(cloudemu.NewAzure().SnapshotServices())), []string{"blobstorage", "cosmosdb", "iam", "keyvault", "virtualmachines"}},
+		{"gcp", asSet(keysOf(cloudemu.NewGCP().SnapshotServices())), []string{"firestore", "gce", "gcs", "iam", "secretmanager"}},
 	}
 
 	for _, tc := range tests {
