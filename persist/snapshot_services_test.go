a36a150679045ec01cd0e80c8ce929e0e5c6fd86
Nitin Kumar <nitinraj844126@gmail.com>
2026-08-27 09:22:34 +0530

    feat(persist): Snapshottable for DNS across AWS/Azure/GCP (#582 wave 4)

--- persist/snapshot_services_test.go
+++ persist/snapshot_services_test.go
@@ -23,9 +23,9 @@ func TestSnapshotServicesDiscovery(t *testing.T) {
 		services map[string]struct{}
 		want     []string
 	}{
-		{"aws", asSet(keysOf(cloudemu.NewAWS().SnapshotServices())), []string{"dynamodb", "ec2", "s3", "secretsmanager"}},
-		{"azure", asSet(keysOf(cloudemu.NewAzure().SnapshotServices())), []string{"blobstorage", "cosmosdb", "keyvault", "virtualmachines"}},
-		{"gcp", asSet(keysOf(cloudemu.NewGCP().SnapshotServices())), []string{"firestore", "gce", "gcs", "secretmanager"}},
+		{"aws", asSet(keysOf(cloudemu.NewAWS().SnapshotServices())), []string{"dynamodb", "ec2", "route53", "s3", "secretsmanager"}},
+		{"azure", asSet(keysOf(cloudemu.NewAzure().SnapshotServices())), []string{"blobstorage", "cosmosdb", "dns", "keyvault", "virtualmachines"}},
+		{"gcp", asSet(keysOf(cloudemu.NewGCP().SnapshotServices())), []string{"clouddns", "firestore", "gce", "gcs", "secretmanager"}},
 	}
 
 	for _, tc := range tests {
